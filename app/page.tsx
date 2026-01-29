9c100e67d39cc438ed1b1a95bd76b14fbac06076
OSEI <Sparkle-47@users.noreply.github.com>
2026-01-29 22:45:33 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+export default function LoginPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Gatherly にログイン</h1>
+      <p>幹事のためのイベント管理ツール</p>
+
+      <a href="/login">
+        <button>ログイン画面へ</button>
+      </a>
+
+
+      <br />
+
+      <input placeholder="メールアドレス" />
+      <br /><br />
+      <input type="password" placeholder="パスワード" />
+      <br /><br />
+      <button>ログイン</button>
+
+      <p style={{ marginTop: "20px", fontSize: "12px" }}>
+        ※ 今は見た目だけです
+      </p>
+    </main>
+  );
+}
